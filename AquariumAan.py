a90cddf9b4f8ede8e6c73315fecc85504153c3e6
vincentrouwhorst <rouwhorst@yahoo.com>
2022-05-31 23:27:56 +0200

    Add Aquarium Scripts

---
+++ AquariumAan.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+#--------------------------------------
+#
+#              AquariumAan.py
+#  Switch Hue Light On in 45 min
+#
+# Author : Vincent Rouwhorst
+# Date   : 31/05/2022
+#
+# Domoticz Json documentation
+# https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Custom_Sensor
+#--------------------------------------
+
+import requests
+import time
+
+DOMOTICZ_IP = 'http://127.0.0.1:8080'
+
+id_name = { 7297 : {"name" : "JMB-C1" },
+            7298 : {"name" : "JMB-C2" },
+            7299 : {"name" : "JMB-C3" },
+            7300 : {"name" : "JMB-C4" },
+            7301 : {"name" : "JMB-C5" }}
+
+def PushCommand(idx, levelx):
+    print(DOMOTICZ_IP + "/json.htm?type=command&param=switchlight&idx=" + str(idx) + "&switchcmd=Set%20Level&level=" + str(levelx))
+    r = requests.get(DOMOTICZ_IP + "/json.htm?type=command&param=switchlight&idx=" + str(idx) + "&switchcmd=Set%20Level&level=" + str(levelx))
+    siteresponse = r.json()
+    if siteresponse["status"] == 'ERR':
+        # Write ERROR to Domoticz log
+        message = "ERROR writing lamp script Aquarium " + str(idx)
+        #print(message)
+        requests.get(DOMOTICZ_IP + "/json.htm?type=command&param=addlogmessage&message=" + message)
+
+
+if __name__ == '__main__':
+  # Script has been called directly
+  countdowntimer = 2700 # 45 min = 2700 sec
+  lamplevel = 101 # 0-100
+
+  for x in range(lamplevel):  # range=On, reversed=Off script
+      for z in id_name.keys():
+          PushCommand(z, x)
+      time.sleep(countdowntimer/lamplevel)
