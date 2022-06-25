fc709800539bc1fa85959417c41fc92bc9acfed1
vincentrouwhorst <rouwhorst@yahoo.com>
2022-06-26 00:12:45 +0200

    Small update

---
+++ HueSensorAan.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+#--------------------------------------
+#
+#              HueSensor.py
+#  Switch Hue Sensor On
+#
+# Author : Vincent Rouwhorst
+# Date   : 25/06/2022
+#
+#--------------------------------------
+
+import requests
+import time
+
+HUE_IP = 'http://192.168.5.28/api/hqJbo66WmQkEtRx3NA5qIgNuZBOhBsAye114-7EH/sensors/19'
+
+
+def ReadCommand():
+    print(HUE_IP)
+    r = requests.get(HUE_IP)
+    siteresponse = r.json()
+    print(siteresponse)
+
+def PutCommand():
+    # Making a PUT request
+    print(HUE_IP)
+    data = '{"config": {"on":true }}'
+    print(data)
+    r = requests.put(HUE_IP, data)
+    # check status code for response received
+    # success code - 200
+    print(r)
+    # print content of request
+    print(r.content)
+
+
+
+if __name__ == '__main__':
+  # Script has been called directly
+    #ReadCommand()
+    PutCommand()
